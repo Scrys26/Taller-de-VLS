9e6aa5fd4844b72ff69e93f1e2b247add8beafb5
Scrys26 <randysteve500@gmail.com>
2026-04-29 19:18:22 -0600

    pass

--- src/mux_2to1_4b.sv
+++ src/mux_2to1_4b.sv
@@ -1,15 +1,26 @@
+/*
+* Instituto Tecnológico de Costa Rica
+* Prof. Dr.-ing. Pablo Mendoza Ponce
+* Rev. 1 28 July 2024
+*/
+
 timescale 1ns/1ps
-
 module mux_2to1_4b(
+  input logic [3:0]  a_i,
+  input logic [3:0]  b_i,
+  input logic        s_i,
+  output logic [3:0] q_o);
 
-input logic [3:0] a_i,
 
-input logic [3:0] b_i,
-
-input logic s_i,
-
-output logic [3:0] q_o);
-
+  
+    always_comb begin
+        if(s_i == 1'b0) begin
+            q_o = a_i;
+        end else begin
+            q_o = b_i;
+        end
+    end
+  
   //<<<<<<<<<<<< HDL DESCRIPTION HERE >>>>>>>>>>>>>>>>>
   
 endmodule
