6298db8ddcf447bb11c4444dfb2cb4f007a0ea52
Scrys26 <randysteve500@gmail.com>
2026-04-29 19:25:22 -0600

    Prueba_Mux

--- src/mux_2to1_4b.v
+++ src/mux_2to1_4b.v
@@ -1,18 +1,24 @@
+// Code your design here
 /*
 * Instituto Tecnológico de Costa Rica
 * Prof. Dr.-ing. Pablo Mendoza Ponce
 * Rev. 1 28 July 2024
 */
 
-timescale 1ns/1ps
+`timescale 1ns/1ps
 
 module mux_2to1_4b(
-  input logic [3:0]  a_i,
-  input logic [3:0]  b_i,
-  input logic        s_i,
-  output logic [3:0] q_o);
+    input  [3:0] a_i,
+    input  [3:0] b_i,
+    input        s_i,
+    output reg [3:0] q_o
+);
 
-  
-  //<<<<<<<<<<<< HDL DESCRIPTION HERE >>>>>>>>>>>>>>>>>
-  
+    always @(*) begin
+        if(s_i == 1'b0)
+            q_o = a_i;
+        else
+            q_o = b_i;
+    end
+
 endmodule
