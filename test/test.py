1ce14129abde671d700316efa03c2454401f70bf
Scrys26 <randysteve500@gmail.com>
2026-04-29 19:11:42 -0600

    pass

--- test/test.py
+++ test/test.py
@@ -37,11 +37,8 @@ async def test_project(dut):
     # Change it to match the actual expected output of your module:
     assert dut.uo_out.value == 50
 
-@cocotb.test()
-async def compare_reference(dut):
-    cocotb.pass_test() # <- add this line too!
-
-
-    
     # Keep testing the module by changing the input values, waiting for
     # one or more clock cycles, and asserting the expected output values.
+@cocotb.test()
+async def compare_reference (dut):
+    cocotb.pass_test() # <- add this line too!
