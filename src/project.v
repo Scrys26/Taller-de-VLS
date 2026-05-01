061d97eddcaf22377c3e4ec8a487c07164101bb0
Scrys26 <randysteve500@gmail.com>
2026-04-30 18:02:10 -0600

    Prueba_Mux

--- src/project.v
+++ src/project.v
@@ -19,30 +19,30 @@ module tt_um_example (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out [7:0]=0 ;  // Example: ou_out is the sum of ui_in and uio_in
+  assign uo_out [7:4]=0 ;  // Example: ou_out is the sum of ui_in and uio_in
   assign uio_out = 0;
-  assign uio_oe = 0;
+  assign uio_oe [7:1] = 0;
 
   // List all unused inputs to prevent warnings
-  //wire _unused = &{ena, clk, rst_n, uio_in [7:1], 1'b0};
+  wire _unused = &{ena, clk, rst_n, uio_in [7:1], 1'b0};
 
-  wire _unused = &{ena , uio_in [7:1], 1'b0};
+ // wire _unused = &{ena , uio_in [7:1], 1'b0};
   assign uio_oe [0]= 1'b0;
 
-  /*mux_2to1_4b U0(
+mux_2to1_4b U0(
     .a_i(ui_in [3:0]),
     .b_i(ui_in [7:4]),
     .s_i(uio_in [0]),
     .q_o(uo_out [3:0]));
- */
 
+/*
 reg_pp_8b_en_ar U0(
   .clck_i(clk),
   .rst_n_i(rst_n),
   .d_i(ui_in[7:0]),
   .en_i(uio_in[0]),
   .q_o(uo_out[7:0])
-
 );
+ */
 
 endmodule
