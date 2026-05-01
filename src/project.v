e27981bbed65fcc5b16e2692948210e0050dcd5a
Scrys26 <randysteve500@gmail.com>
2026-04-30 20:47:31 -0600

    Prueba UART

--- src/project.v
+++ src/project.v
@@ -23,42 +23,42 @@ module tt_um_example (
   //assign uio_out = 0;
   //assign uio_oe [7:1] = 0;
 
-
+  assign reset = ~rst_n;
   assign uio_out = 0;
   assign uio_oe = 0;
 
   // List all unused inputs to prevent warnings
   //wire _unused = &{ena, clk, rst_n, uio_in [7:1], 1'b0};
 
-  wire _unused = &{ena , uio_in [7:0], ui_in, [7:1], 1'b0};
-  assign uio_oe [0]= 1'b0;
+  wire _unused = &{ena, ui_in[7:1], uio_in, 1'b0};
 
-// wire para interconectar
+  // wire para interconectar
 
-wire uart_reg [7:0] = 0;
-wire uart_rdy = 0;
+  wire [7:0] uart_data;
+  wire       uart_rdy;
+  wire       reset;
 
-/*mux_2to1_4b U0(
-    .a_i(ui_in [3:0]),
-    .b_i(ui_in [7:4]),
-    .s_i(uio_in [0]),
-    .q_o(uo_out [3:0]));
-*/
+  /*mux_2to1_4b U0(
+      .a_i(ui_in [3:0]),
+      .b_i(ui_in [7:4]),
+      .s_i(uio_in [0]),
+      .q_o(uo_out [3:0]));
+  */
 
-reg_pp_8b_en_ar U0(
-  .clk_i(clk),
-  .rst_n_i(rst_n),
-  .d_i(uart_reg),
-  .en_i(wire uart_rdy),
-  .q_o(uo_out[7:0])
-);
+  UART_rx U1 (
+    .clk         (clk),
+    .reset       (reset),
+    .rx_data_in  (ui_in[0]),
+    .rx_data_rdy (uart_rdy),
+    .rx_data_out (uart_data)
+  );
 
-UART_rx U1(
-  .clk          (clk), 
-  .reset        (rst_n), 
-  .rx_data_in   (ui_in[0]), 
-  .rx_data_rdy  (wire uart_rdy), 
-  .rx_data_out  (uart_reg)
+  reg_pp_8b_en_ar U0 (
+    .clk_i   (clk),
+    .rst_n_i (rst_n),
+    .d_i     (uart_data),
+    .en_i    (uart_rdy),
+    .q_o     (uo_out)
   );
 
 endmodule
